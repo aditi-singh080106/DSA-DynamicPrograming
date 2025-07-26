b4dcb64b75be58decf90ff2780159b40b2f89fc5
Aditi Singh <aditi08092003@gmail.com>
2025-07-26 14:15:58 +0530

    Update FriendPairTabulation.java

--- FriendPairTabulation.java
+++ FriendPairTabulation.java
@@ -6,7 +6,8 @@ class Main {
         System.out.println("Enter number of friends : ");
         int frnd = sc.nextInt();
         int[] dp = new int[frnd+1];
-        dp[1] = 1 ; dp[2] = 2 ;
+        dp[1] = 1 ;
+        if(n >= 1)dp[2] = 2 ;
         for(int i = 3 ; i <= frnd ; i++ ){
             dp[i] = dp[i-1] + (i-1)*dp[i-2];
         }
